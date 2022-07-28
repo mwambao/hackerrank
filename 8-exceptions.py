d40fe97b1d531a5312e314ad65a533168d7c1e77
Duncan O. Mwamba <duncan.owino@gmail.com>
2022-07-28 21:41:23 +0100

    UPDATES

--- 8-exceptions.py
+++ 8-exceptions.py
@@ -10,6 +10,7 @@ This error is raised when the second argument of a division or modulo operation 
 >>> b = '0'
 >>> print int(a) / int(b)
 >>> ZeroDivisionError: integer division or modulo by zero
+
 ValueError
 This error is raised when a built-in operation or function receives an argument that has the right type but an inappropriate value.
 
@@ -33,19 +34,19 @@ Error Code: integer division or modulo by zero
 
 Task
 
-You are given two values  and .
-Perform integer division and print .
+You are given two values a and b.
+Perform integer division and print a/b.
 
 Input Format
 
-The first line contains , the number of test cases.
-The next  lines each contain the space separated values of  and .
+The first line contains T, the number of test cases.
+The next  lines each contain the space separated values of a and b.
 
 Constraints
-
+0 < T < 10
 Output Format
 
-Print the value of .
+Print the value of a/b.
 In the case of ZeroDivisionError or ValueError, print the error code.
 
 Sample Input
@@ -54,11 +55,14 @@ Sample Input
 1 0
 2 $
 3 1
+
 Sample Output
 
 Error Code: integer division or modulo by zero
 Error Code: invalid literal for int() with base 10: '$'
 3
+
 Note:
 For integer division in Python 3 use //.
+SOLUTION
 '''
